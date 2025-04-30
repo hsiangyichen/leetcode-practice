a73e15d010d29f78c628ec25c631b1733fb32719
hsiangyichen <hsiangyi1025@gmail.com>
2025-04-29 19:46:14 -0700

    isAnagram

--- isAnagram.py
+++ isAnagram.py
@@ -1,22 +1,25 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        # input: 2 strings; output: boolean
+# Question: Given two strings s and t, return true if t is an anagram of s and false otherwise.
+# Input: s = "anagram", t = "nagaram"
+# Output: true
 
-        # Method 1:
-        # loop through the first str and turn it into a dict
-        # loop through the second str and turn it into a dict
-        # check if any value is different
-            # if yes, return false
-        # return true
+# def isAnagram(self, s, t):
+    # input: 2 strings; output: boolean
 
-        # Method 2:
-        # loop through both strings
-            # if it is from t, +1
-            # if it is from s, -1
-        # check if all the value is 0
-            # if not, return false
-        # return true
+    # Method 1:
+    # loop through the first str and turn it into a dict
+    # loop through the second str and turn it into a dict
+    # check if any value is different
+        # if yes, return false
+    # return true
 
-        # Method 3:
-        # use Counter
-        # return Counter(s) == Counter(t) 
+    # Method 2:
+    # loop through both strings
+        # if it is from t, +1
+        # if it is from s, -1
+    # check if all the value is 0
+        # if not, return false
+    # return true
+
+    # Method 3:
+    # use Counter
+    # return Counter(s) == Counter(t) 
