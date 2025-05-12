630190089d7d91336b4d74d7387638a4d2e3c2cb
hsiangyichen <hsiangyi1025@gmail.com>
2025-05-12 00:52:19 -0700

    isPalindrome

---
+++ isPalindrome.py
@@ -0,0 +1,16 @@
+# Question: Given the head of a singly linked list, return true if it is a palindrome or false otherwise.
+# Input: head = [1,2,2,1]
+# Output: true
+# Input: head = [1,2]
+# Output: false
+
+# def isPalindrome(self, head):
+    # result = []
+    # curr = head
+
+    # while curr:
+    #     result.append(curr.val)
+    #     curr = curr.next
+    
+    # return result == result[::-1]
+        
