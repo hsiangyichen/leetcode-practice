3667247f4c68b42f237b39cb4d82253225a53012
hsiangyichen <hsiangyi1025@gmail.com>
2025-05-22 22:24:54 -0700

    maxProfit

--- maxProfit.py
+++ maxProfit.py
@@ -9,15 +9,33 @@
     
     # Method 1: O(n^2)
     # loop through the list
-        # loop through the rest of the list
-            # profit = list[j] - list[i]
-            # check if profit > max_profit:
-                # max_profit = profit
+    #     loop through the rest of the list
+    #         profit = list[j] - list[i]
+    #         check if profit > max_profit:
+    #             max_profit = profit
+    # return max_profit
+    # max_profit = 0
+    # for i in range(len(prices)):
+    #     for j in range(i+1, len(prices)):
+    #         profit = prices[j] - prices[i]
+    #         if profit > max_profit:
+    #             max_profit = profit
+    
     # return max_profit
 
     # Method 2: O(n)
     # set min_price to the first price
     # loop through the rest of the list
-            # if price < min_price, then replace min_price
-        # if price - min_price > max_profit, then replace max_profit
+    #         if price < min_price, then replace min_price
+    #     if price - min_price > max_profit, then replace max_profit
+    # return max_profit
+
+    # buy = prices[0]
+    # max_profit = 0
+    # for price in prices[1:]:
+    #     if price < buy:
+    #         buy = price
+    #     profit = price - buy
+    #     if profit > 0 and profit > max_profit:
+    #         max_profit = profit
     # return max_profit
